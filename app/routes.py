9ed826a764e0d4fa3c3bad17990855c091bca465
Dion Dresschers <dion@dresschers.net>
2021-03-11 12:42:45 +0100

    2. Templates en Loops in templates en extends

--- app/routes.py
+++ app/routes.py
@@ -7,4 +7,14 @@ def index():
     user2 = {                # P12. Dummy-data.
         "username":"Dion", 
     }
-    return render_template('index.html', user=user2) # The first user is the `user` is the `user` in the template, the second is the `user`-variable defined above.
+    posts2 = [ # This is a Python3 'list', a tuple will be within paranthesis `()``.
+        {
+            'author' : {'username' : 'Mimi'},
+            'body' : "Oh it's such a perfect day!",
+        },
+        {
+            'author' : {'username': "Peter"}, 
+            'body' : "Je suis, Jackshon...",
+        },   
+    ]
+    return render_template('index.html', user=user2, title="Homepagina", posts=posts2) # The first user is the `user` is the `user` in the template, the second is the `user`-variable defined above.
