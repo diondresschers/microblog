62da60759a6bb16972cad1e52c94fe7470a38b27
Dion Dresschers <dion@dresschers.net>
2021-03-11 12:27:08 +0100

    With a template and with dummy data

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,10 @@
+from flask import render_template
 from app import app # The first `app`, is the `app`-directory. The second `app` is the `app` created in the `__init__.py`-file: `app = Flask(__name__)`.
 
 @app.route('/') # P.6 - These are 'decorators'.
 @app.route('/index') # P.6 - You can chain decorators.
 def index():
-    user = {                # P12. Dummy-data.
+    user2 = {                # P12. Dummy-data.
         "username":"Dion", 
     }
-    return """
-<html>
-    <head>
-        <title>Homepage</title>
-    </head>
-    <body>
-        <h1>Hello, """ + user['username'] + """!        
-        </h1>
-    </body>
-</html>
-"""
-
+    return render_template('index.html', user=user2) # The first user is the `user` is the `user` in the template, the second is the `user`-variable defined above.
---
+++ app/templates/_templates.md
@@ -0,0 +1,1 @@
+P13. In Flask, temapltes are stored in a templates folder called `templates` that is inside the application folder.
