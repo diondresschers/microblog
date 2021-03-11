ae144b92304ffb42556d28235c09cf2fdee95386
Dion Dresschers <dion@dresschers.net>
2021-03-11 17:39:16 +0100

    Added error messages in WTF in the template

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 # P.25 User Login Form
 
-from flask_wtf import FlaskForm         # Most Flask extensions use a `flask_`-naming convention
+from flask_wtf import FlaskForm         # Most Flask extensions use a `flask_`-naming convention # wtf_forms is pronounces as 'Witty Forms`
 
 from wtforms import StringField
 from wtforms import PasswordField
@@ -9,8 +9,9 @@ from wtforms import SubmitField
 
 from wtforms.validators import DataRequired
 
-class LoginForm(Flaskform):
-    username = StringField('Username', validators=[DataRequired])
-    password = StringField('Password', validators=[DataRequired])
+class LoginForm(FlaskForm): 
+    username = StringField('Username', validators=[DataRequired()]) # The first argument 'Uername' will be the `.label`
+    password = StringField('Password', validators=[DataRequired()]) # Don't forget the (), otherwise the error might be "TypeError: <class 'wtforms.validators.DataRequired'> is not a valid validator because it is a class, it should be an instance"
     remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign in')
+    submit = SubmitField('Sign in') # The argument is the label that will be displayed in the button.
+
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
 from flask import render_template
-from app import app # The first `app`, is the `app`-directory. The second `app` is the `app` created in the `__init__.py`-file: `app = Flask(__name__)`.
+from flask import flash             # P.28 for Flask messages. 
+from flask import redirect          # Redeirect when a form is succesfully entered, redirect to another page.
 
+from app import app # The first `app`, is the `app`-directory. The second `app` is the `app` created in the `__init__.py`-file: `app = Flask(__name__)`.
+from app.forms import LoginForm # De `app`, is de `app`-directory, de `.forms` is de `forms.py`-file en de `LoginForm` is de Class in die file
 @app.route('/') # P.6 - These are 'decorators'.
 @app.route('/index') # P.6 - You can chain decorators.
 def index():
@@ -18,3 +21,12 @@ def index():
         },   
     ]
     return render_template('index.html', user=user2, title="Homepagina", posts=posts2) # The first user is the `user` is the `user` in the template, the second is the `user`-variable defined above.
+
+@app.route('/login', methods=['GET','POST']) # P.29, accept the 'GET' and the 'POST' methods for forms.
+def login():
+    form2 = LoginForm() # P.27 Deze moeten we daadwerkerlijk doorgeven!
+    # Check if the form is submitted or not:
+    if form2.validate_on_submit(): # This is a function from Witty-forms. If the Submit-button have been pushed, so when this is a POST in stead of a GET
+        flash(f'Login requested for user {form2.username.data}, {form2.remember_me.data}') # P.29 The `data` is the data that came with the form that the user just added. When you call the flash() message this will be stored, but you also have to show them in the template!
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form2)
